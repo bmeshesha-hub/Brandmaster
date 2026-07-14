7a42f5c854f5918bc13e10819ac18c2bd76ba804
bmeshesha <bmeshesha@ebay.com>
2026-07-14 16:42:50 -0400

    Deploy Brandmaster to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
-const CACHE = "brandmaster-static-1784052458483";
+const CACHE = "brandmaster-static-1784061769116";
 const BASE = "/Brandmaster";
 const PRECACHE = [
   "/Brandmaster/404.html",
   "/Brandmaster/404/index.html",
-  "/Brandmaster/_next/static/Qv4AlJn3tN8cgo0LaA4tE/_buildManifest.js",
-  "/Brandmaster/_next/static/Qv4AlJn3tN8cgo0LaA4tE/_ssgManifest.js",
   "/Brandmaster/_next/static/chunks/255-12546c87896b2090.js",
   "/Brandmaster/_next/static/chunks/4bd1b696-c023c6e3521b1417.js",
   "/Brandmaster/_next/static/chunks/668-63bcb556766d8fa5.js",
   "/Brandmaster/_next/static/chunks/app/_not-found/page-d51e196185abbbfa.js",
   "/Brandmaster/_next/static/chunks/app/layout-26d3f14294bc53a2.js",
-  "/Brandmaster/_next/static/chunks/app/page-3a09b352814b8bff.js",
+  "/Brandmaster/_next/static/chunks/app/page-8a3654524fcf0d74.js",
   "/Brandmaster/_next/static/chunks/framework-2c534e0e662575a2.js",
   "/Brandmaster/_next/static/chunks/main-app-3485f854aba1ca97.js",
   "/Brandmaster/_next/static/chunks/main-b5a943307cece8e0.js",
@@ -18,7 +16,9 @@ const PRECACHE = [
   "/Brandmaster/_next/static/chunks/pages/_error-cb2a52f75f2162e2.js",
   "/Brandmaster/_next/static/chunks/polyfills-42372ed130431b0a.js",
   "/Brandmaster/_next/static/chunks/webpack-c985628553d94265.js",
-  "/Brandmaster/_next/static/css/020b5de08a4b84eb.css",
+  "/Brandmaster/_next/static/css/ca4a0c7f2e64c412.css",
+  "/Brandmaster/_next/static/zoiTMoLdCu-cxilBaTXdu/_buildManifest.js",
+  "/Brandmaster/_next/static/zoiTMoLdCu-cxilBaTXdu/_ssgManifest.js",
   "/Brandmaster/icon.svg",
   "/Brandmaster/index.html",
   "/Brandmaster/index.txt",
