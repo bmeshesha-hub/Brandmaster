d81eb6dec60722c52c7a2c4dda78a4ae352fe09a
bmeshesha <bmeshesha@ebay.com>
2026-07-22 17:01:40 -0400

    Deploy Brandmaster to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE = "brandmaster-static-1784750879247";
+const CACHE = "brandmaster-static-1784754099175";
 const BASE = "/Brandmaster";
 const PRECACHE = [
   "/Brandmaster/404.html",
   "/Brandmaster/404/index.html",
-  "/Brandmaster/_next/static/BfUr8WkvCT-o7_FfeHg5X/_buildManifest.js",
-  "/Brandmaster/_next/static/BfUr8WkvCT-o7_FfeHg5X/_ssgManifest.js",
   "/Brandmaster/_next/static/chunks/164f4fb6.ca8844c7aa0d818b.js",
   "/Brandmaster/_next/static/chunks/199.829cec104a19a84e.js",
   "/Brandmaster/_next/static/chunks/214-e5ae6fff17b26027.js",
@@ -18,9 +16,9 @@ const PRECACHE = [
   "/Brandmaster/_next/static/chunks/ad2866b8.635304a38afc0b68.js",
   "/Brandmaster/_next/static/chunks/app/_not-found/page-75050ae25a55d1a2.js",
   "/Brandmaster/_next/static/chunks/app/analytics/page-3e4f1fdac3eda06b.js",
-  "/Brandmaster/_next/static/chunks/app/global-error-c23f5a0139f7ea12.js",
+  "/Brandmaster/_next/static/chunks/app/global-error-24ddd2cc03109c53.js",
   "/Brandmaster/_next/static/chunks/app/layout-0e7ecc78d624b368.js",
-  "/Brandmaster/_next/static/chunks/app/page-bf7ef0e45640b984.js",
+  "/Brandmaster/_next/static/chunks/app/page-fa7d53ead24fc6e8.js",
   "/Brandmaster/_next/static/chunks/bc98253f.d6fc8a0138855acd.js",
   "/Brandmaster/_next/static/chunks/framework-f52ebcb9f26a1e11.js",
   "/Brandmaster/_next/static/chunks/main-10008fbc38c32a00.js",
@@ -29,7 +27,9 @@ const PRECACHE = [
   "/Brandmaster/_next/static/chunks/pages/_error-022e4ac7bbb9914f.js",
   "/Brandmaster/_next/static/chunks/polyfills-42372ed130431b0a.js",
   "/Brandmaster/_next/static/chunks/webpack-870a38a3da162a0e.js",
-  "/Brandmaster/_next/static/css/97b4de0a28666203.css",
+  "/Brandmaster/_next/static/css/6feb365759d62005.css",
+  "/Brandmaster/_next/static/fhUAnoxYfKlacnThiqjsY/_buildManifest.js",
+  "/Brandmaster/_next/static/fhUAnoxYfKlacnThiqjsY/_ssgManifest.js",
   "/Brandmaster/analytics-snapshot.json",
   "/Brandmaster/analytics/index.html",
   "/Brandmaster/analytics/index.txt",
