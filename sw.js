379767da70105c1803f069fcbc54f80bd3309afd
bmeshesha <bmeshesha@ebay.com>
2026-07-23 13:18:49 -0400

    Deploy Brandmaster to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE = "brandmaster-static-1784822837812";
+const CACHE = "brandmaster-static-1784827128976";
 const BASE = "/Brandmaster";
 const PRECACHE = [
   "/Brandmaster/404.html",
   "/Brandmaster/404/index.html",
-  "/Brandmaster/_next/static/OoCO1xz59ZlXYw-pZTA9F/_buildManifest.js",
-  "/Brandmaster/_next/static/OoCO1xz59ZlXYw-pZTA9F/_ssgManifest.js",
   "/Brandmaster/_next/static/chunks/164f4fb6.ca8844c7aa0d818b.js",
   "/Brandmaster/_next/static/chunks/199.829cec104a19a84e.js",
   "/Brandmaster/_next/static/chunks/214-e5ae6fff17b26027.js",
@@ -20,7 +18,7 @@ const PRECACHE = [
   "/Brandmaster/_next/static/chunks/app/analytics/page-3e4f1fdac3eda06b.js",
   "/Brandmaster/_next/static/chunks/app/global-error-24ddd2cc03109c53.js",
   "/Brandmaster/_next/static/chunks/app/layout-0e7ecc78d624b368.js",
-  "/Brandmaster/_next/static/chunks/app/page-7a9f941e5b1f42a9.js",
+  "/Brandmaster/_next/static/chunks/app/page-16184765da98d451.js",
   "/Brandmaster/_next/static/chunks/bc98253f.d6fc8a0138855acd.js",
   "/Brandmaster/_next/static/chunks/framework-f52ebcb9f26a1e11.js",
   "/Brandmaster/_next/static/chunks/main-10008fbc38c32a00.js",
@@ -30,13 +28,16 @@ const PRECACHE = [
   "/Brandmaster/_next/static/chunks/polyfills-42372ed130431b0a.js",
   "/Brandmaster/_next/static/chunks/webpack-870a38a3da162a0e.js",
   "/Brandmaster/_next/static/css/bae4b54e22e3232e.css",
+  "/Brandmaster/_next/static/oR1SVL9v5ImhWmwS4wkHz/_buildManifest.js",
+  "/Brandmaster/_next/static/oR1SVL9v5ImhWmwS4wkHz/_ssgManifest.js",
   "/Brandmaster/analytics-snapshot.json",
   "/Brandmaster/analytics/index.html",
   "/Brandmaster/analytics/index.txt",
   "/Brandmaster/icon.svg",
   "/Brandmaster/index.html",
   "/Brandmaster/index.txt",
-  "/Brandmaster/manifest.webmanifest"
+  "/Brandmaster/manifest.webmanifest",
+  "/Brandmaster/manual-fpa-current.json"
 ];
 
 self.addEventListener("install", (event) => {
