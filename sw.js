ffd03eb10017b97658f1dc80ac483fb6eb9577b3
bmeshesha <bmeshesha@ebay.com>
2026-09-10 11:20:43 -0400

    Deploy Brandmaster to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Hosted build generated 2026-09-10T15:18:09.560Z. This worker retires
+// Hosted build generated 2026-09-10T15:20:42.315Z. This worker retires
 // older offline installations without touching localStorage or IndexedDB data.
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
